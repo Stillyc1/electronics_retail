2be7f0bfc591dc3864fce6fed74c7c80b2bae113
Alexander_Baranov <Sti_ll@mail.ru>
2025-04-07 20:22:14 +0300

    add method save_model

--- users/admin.py
+++ users/admin.py
@@ -11,14 +11,7 @@ class UserAdmin(admin.ModelAdmin):
     list_filter = ("username",)
     search_fields = ("id", "username",)
 
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        if request.user.is_superuser:
-            return qs
-        return qs.none()
-
     def save_model(self, request, obj, form, change):
-        """Хэшируем пароль при создании сотрудника через админ-панель."""
-        if form.cleaned_data['password']:
-            obj.set_password(form.cleaned_data['password'])
-        super().save_model(request, obj, form, change)
+        if not change:
+            obj.set_password(form.cleaned_data["password"])
+        obj.save()
